b2e2973aab11416ebec99ca47df48c763447a791
Jeevan HR <jeevanhr7@gmail.com>
2016-04-01 09:24:23 +0530

    Updated the Language

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/about',middleware.logger, function (req, res) {
-    res.send("About us");
+    res.send("About us !");
 });
 app.use(express.static(__dirname + '/public'))
 
